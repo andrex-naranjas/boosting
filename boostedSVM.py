399046b563d0196f2fdfec6ddb35e65b8bef4f38
Jorge <jorge.salmon@kcl.ac.uk>
2020-02-24 16:14:57 +0000

    AdaBoost and Neural Networks classifiers added.

--- boostedSVM.py
+++ boostedSVM.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 15:09:24 2020
+
+@author: tokam
+"""
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -22,14 +29,20 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
+
+# metrics
+from sklearn.metrics import accuracy_score
 
 # import module for data preparation
 import data_preparation as dp
 
 if len(sys.argv) != 2:
-    sys.exit('Please provide which sample name to analyse. Try again!') 
+    sys.exit('Please provide which sample name to analyse. Try again!')
 
 sample = sys.argv[1]
+
 
 # fetch data
 if sample == 'titanic':
@@ -59,7 +72,7 @@ elif sample == 'cancer':
     X,Y = dp.bCancer(data_set)
 elif sample == 'german':
     X,Y = dp.german(data_set)
-    
+
 # print data after preparation
 print("After preparation", data_set.shape)
 print(X.head())
@@ -74,13 +87,27 @@ print(X_test.shape, Y_test.shape)
 svc = SVC()
 svc.fit(X_train, Y_train)
 Y_pred = svc.predict(X_test)
-acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+acc_svc = round(accuracy_score(Y_test, Y_pred) * 100, 2)
 print(acc_svc, 'svm result')
 
-# random forest (Boosted decision trees)
+# random forest classifier
 random_forest = RandomForestClassifier(n_estimators=100)
 random_forest.fit(X_train, Y_train)
 Y_pred = random_forest.predict(X_test)
-random_forest.score(X_train, Y_train)
-acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
-print(acc_random_forest, 'BDT result')
+acc_random_forest = round(accuracy_score(Y_test, Y_pred) * 100, 2)
+print(acc_random_forest, 'RFC result')
+
+# AdaBoost This class implements the algorithm known as AdaBoost-SAMME:
+# Zhu, H. Zou, S. Rosset, T. Hastie, “Multi-class AdaBoost”, 2009.
+AdaBoost = AdaBoostClassifier(n_estimators=100, random_state=0)
+AdaBoost.fit(X_train, Y_train)
+Y_pred = AdaBoost.predict(X_test)
+acc_AdaBoost = round(accuracy_score(Y_test, Y_pred) * 100, 2)
+print(acc_AdaBoost, 'AdaBoost result')
+
+# Neural Network Multi Layer perceptron classifier
+NeuralNet = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+NeuralNet.fit(X_train, Y_train)
+Y_pred = NeuralNet.predict(X_test)
+acc_NeuralNet = round(accuracy_score(Y_test, Y_pred) * 100, 2)
+print(acc_AdaBoost, 'Neural Network result')
