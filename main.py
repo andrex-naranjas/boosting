214bf0f6db3ba687f4402ef7eb25ebb556dee096
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-03-05 12:11:58 +0900

    Update

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ for name in sample_list:
     #ss.stats_results(name, n_cycles=5, kfolds=3, n_reps=2, boot_kfold ="bootstrap")
     ss.stats_results(name, n_cycles=100, kfolds=10, n_reps=10, boot_kfold ="bootstrap")
     # kfold cross-validation
-    ss.stats_results(name, n_cycles=10, kfolds=10, n_reps=10, boot_kfold ="kfold")    
+    ss.stats_results(name, n_cycles=10, kfolds=20, n_reps=10, boot_kfold ="kfold")    
     end = datetime.datetime.now()
     elapsed_time = end - start
     print("Elapsed total time = " + str(elapsed_time))
