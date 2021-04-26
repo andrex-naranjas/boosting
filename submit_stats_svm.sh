13af332b0fdf9cc72d673c9481736f911d33b5da
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-04-26 17:58:46 +0900

    Add options sample

--- submit_stats_svm.sh
+++ submit_stats_svm.sh
@@ -1,6 +1,7 @@
+sample_name = sample
 Universe = vanilla
 Executable = /usr/bin/python3
-Arguments = batch_stats_summary.py $(Process)
+Arguments = batch_stats_summary.py $(Process) sample_name
 output = output_batch_german/out.$(Process)
 error = output_batch_german/error.$(Process)
 Log = log.txt
