da132dc39ff619bdb94c18648b5765b569d89835
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-04-26 16:41:19 +0900

    Change config a little

--- submit_stats_svm.sh
+++ submit_stats_svm.sh
@@ -1,7 +1,7 @@
 Universe = vanilla
 Executable = /usr/bin/python3
 Arguments = batch_stats_summary.py $(Process)
-output = output_batch/out.$(Process)
-error = output_batch/error.$(Process)
+output = output_batch_german/out.$(Process)
+error = output_batch_german/error.$(Process)
 Log = log.txt
 Queue 100
