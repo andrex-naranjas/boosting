81d09febf611623cd1e9337f68f7d8b2ddbaecbf
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-04-26 19:06:32 +0900

    test

--- submit_stats_svm.sh
+++ submit_stats_svm.sh
@@ -1,6 +1,6 @@
 Universe = vanilla
 Executable = /usr/bin/python3
-Arguments = batch_stats_summary.py $(Process) "titanic"
+Arguments = batch_stats_summary.py $(Process) $("titanic")
 output = output_batch_test/out.$(Process)
 error = output_batch_test/error.$(Process)
 Log = log.txt
