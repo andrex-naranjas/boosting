e57064af6f6b591146f138bf8bd5cc6cb42c27c8
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-05-20 17:22:10 +0900

    force setup dir

--- setup_dir.py
+++ setup_dir.py
@@ -7,7 +7,7 @@
 from os import system
 
 # create directories
-system('mkdir plots tables')
-system('mkdir output && cd output && mkdir abalone  adult  belle2_i  belle2_ii  belle2_iii  belle2_iv  belle_iii  cancer  car  connect  contra  ecoli  german  heart  solar  tac_toe  titanic  wine && cd ..')
-system('mkdir stats_results && cd stats_results && mkdir abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..')
-system('mkdir output_batch && cd output_batch && mkdir abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..')
+system('mkdir -p plots tables')
+system('mkdir -p output && cd output && mkdir -p abalone  adult  belle2_i  belle2_ii  belle2_iii  belle2_iv  belle_iii  cancer  car  connect  contra  ecoli  german  heart  solar  tac_toe  titanic  wine && cd ..')
+system('mkdir -p stats_results && cd stats_results && mkdir -p abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..')
+system('mkdir -p output_batch && cd output_batch && mkdir -p abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..')
