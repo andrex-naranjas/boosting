5d90164f3e3631dd37a6b21a30cd08abf8401c0e
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-05-20 14:37:28 +0900

    remove first config

--- setup.sh
+++
@@ -1,4 +0,0 @@
-mkdir plots tables
-mkdir output && cd output && mkdir abalone  adult  belle2_i  belle2_ii  belle2_iii  belle2_iv  belle_iii  cancer  car  connect  contra  ecoli  german  heart  solar  tac_toe  titanic  wine && cd ..
-mkdir stats_results && cd stats_results && mkdir abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..
-mkdir output_batch && cd output_batch && mkdir abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..
