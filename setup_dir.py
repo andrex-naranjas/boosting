0fb005e829c4368e1af283421c817f6c9f7d5fd2
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-05-24 19:48:53 +0900

    Update setup dir script

--- setup_dir.py
+++ setup_dir.py
@@ -1,4 +1,4 @@
-'''
+''''
 ---------------------------------------------------------------
  Code to improve SVM
  Authors: A. Ramirez-Morales and J. Salmon-Gamboa
@@ -9,5 +9,17 @@ from os import system
 # create directories
 system('mkdir -p plots tables')
 system('mkdir -p output && cd output && mkdir -p abalone  adult  belle2_i  belle2_ii  belle2_iii  belle2_iv  belle_iii  cancer  car  connect  contra  ecoli  german  heart  solar  tac_toe  titanic  wine && cd ..')
-system('mkdir -p stats_results && cd stats_results && mkdir -p abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..')
+
+system('mkdir -p stats_results && cd stats_results && mkdir -p abalone && cd abalone && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p adult && cd adult && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p cancer && cd cancer && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p car && cd car && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p connect && cd connect && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p ecoli && cd ecoli && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p german && cd german && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p heart && cd heart && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p solar && cd solar && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p titanic && cd titanic && mkdir -p boot kfold')
+system('cd stats_results && mkdir -p wine && cd wine && mkdir -p boot kfold')
+
 system('mkdir -p output_batch && cd output_batch && mkdir -p abalone  adult  cancer  car  connect  ecoli  german  heart  solar  titanic  wine && cd ..')
