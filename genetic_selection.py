edd0bd4ae1252c15ca812396386271e410a00128
Andres Ramirez <andres.ramirez.morales@cern.ch>
2021-11-16 18:37:35 +0900

    fix tournament selection

--- genetic_selection.py
+++ genetic_selection.py
@@ -196,7 +196,7 @@ class genetic_selection:
         for _ in range(2):
             competitors = np.random.choice(indices, size_k, replace=False)
             best_competitor = min(competitors)
-            indices.remove(best_competitor)
+            indices.delete(best_competitor)
             parents_indices.append(best_competitor)
 
         pa_x = popx[parents_indices[0]]
